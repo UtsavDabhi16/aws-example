def84a6d6d5d5cf9970126d652a57060725ddf63
UtsavDabhi16 <utsav.inexture@gmail.com>
2024-03-10 17:05:16 +0530

    get all the buckets from aws

--- src/main/java/com/example/awsexample/Coco.java
+++ src/main/java/com/example/awsexample/Coco.java
@@ -1,14 +1,33 @@
 package com.example.awsexample;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
 
 public class Coco {
+    public static void main(String[] args) {
+        // Replace these values with your AWS access key and secret key
+        String accessKey = "AKIA4MTWMOVSXURQGQ5I";
+        String secretKey = "7WL0IralCob3rT/V/93iMVdHi96zYvgXfJfKK0b1";
 
+        // Set AWS region
+        String region = "us-east-1";
+
+        // Create credentials object
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        // Create Amazon S3 client
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+
+        // List buckets
+        System.out.println("List of S3 buckets:");
+        for (Bucket bucket : s3Client.listBuckets()) {
+            System.out.println(bucket.getName());
+        }
+    }
 }
